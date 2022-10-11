41ec034b6fa5da7f4794a265b70b4df8ebc13cda
KJ Krueger <kjkrueger12@gmail.com>
2022-10-11 12:42:56 -0700

    added middleware

--- server.js
+++ server.js
@@ -6,3 +6,9 @@ console.log(db);
 const PORT = process.env.port || 3001;
 
 const app = express();
+
+
+//adding mIddleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
