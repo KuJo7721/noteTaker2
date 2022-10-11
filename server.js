10e191f714971d4a5271d07e6b86243eae2bfc0c
KJ Krueger <kjkrueger12@gmail.com>
2022-10-11 15:20:38 -0700

    added last api routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 let db = require("./db/db.json");
 console.log(db);
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
 const PORT = process.env.port || 3001;
 
@@ -15,8 +16,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //api routes
-app.get('/api/notes',(req, res)=.{
+app.get('/api/notes',(req, res)=>{
     return res.json(db)
+})
+app.post('/api/notes',(req, res)=>{
+console.log(req.body) 
+req.body.id = uuidv4();
+console.log(req.body)
+db.push(req.body) 
+fs.writeFile('./db/db.json', JSON.stringify(db),(err)=>{
+    if(err)throw err;
+    return res.json(db)
+})  
 })
 
 //html routes
@@ -29,7 +40,7 @@ app.get('/notes', (req, res) =>
 
 //homepage route and wildcard 
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.listen(PORT, () =>
